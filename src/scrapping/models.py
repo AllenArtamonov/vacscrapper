8c06865a757e636adbb0d25770c447686657c664
Allen <allen.artamonov@gmail.com>
2020-12-01 00:21:08 +0200

    slug autogeneration programming language model

--- src/scrapping/models.py
+++ src/scrapping/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from scrapping.utils import from_cyrillic_to_eng
+
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название населенного пункта')
-    slag = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
@@ -12,3 +16,27 @@ class City(models.Model):
         
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
